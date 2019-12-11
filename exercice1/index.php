69403468a295701587c117e128ec1962ac035b9c
thyago_lbt <thyago14@outlook.fr>
2019-12-11 09:24:30 +0100

    first commit

--- exercice1/index.php
+++ exercice1/index.php
@@ -1,13 +1,5 @@
 <?php
 $exerciseNb = 1;
 include '../header.php';
-$start = 0;
-$result = '';
-/* Tant que $start est inférieure | égal a 10 */
-while ($start <= 10) {
-    $result = $start++;  /* La valeur affichée est $start avant l'incrémentation
-                   (post-incrémentation)  */
-}
-?>
-<p><?= $result ?></p>
+ ?>
 <?php include '../footer.php'; ?>
--- header.php
+++ header.php
@@ -3,19 +3,21 @@
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <title>PHP | P3-exercice<?= $exerciseNb ?></title>
+  <title>PHP | Partie 5</title>
 </head>
 <body>
-  <h1 class="text-center text-dark">PHP | P3-ex<?= $exerciseNb ?></h1>
+  <h1 class="text-center text-dark">PHP | P5-ex<?= $exerciseNb ?></h1>
   <div class="container text-center">
     <nav>
-      <a href="http://php-partier3/exercice1/">ex1</a>
-      <a href="http://php-partier3/exercice2/">ex2</a>
-      <a href="http://php-partier3/exercice3/">ex3</a>
-      <a href="http://php-partier3/exercice4/">ex4</a>
-      <a href="http://php-partier3/exercice5/">ex5</a>
-      <a href="http://php-partier3/exercice6/">ex6</a>
-      <a href="http://php-partier3/exercice7/">ex7</a>
-      <a href="http://php-partier3/exercice8/">ex8</a>
+      <a href="http://pt5ex1/">ex1</a>
+      <a href="http://pt5ex2/">ex2</a>
+      <a href="http://pt5ex3/">ex3</a>
+      <a href="http://pt5ex4/">ex4</a>
+      <a href="http://pt5ex5/">ex5</a>
+      <a href="http://pt5ex6/">ex6</a>
+      <a href="http://pt5ex7/">ex7</a>
+      <a href="http://pt5ex8/">ex8</a>
+      <a href="http://pt5ex9/">ex9</a>
+      <a href="http://pt5ex10/">ex10</a>
     </nav>
         <!-- Fin header.php -->
